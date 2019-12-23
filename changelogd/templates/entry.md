7f0e85bea3eb7dc0b62ad858369ae452f65470a2
Andrzej Klajnert <github@aklajnert.pl>
2019-12-23 18:52:19 +0100

    Prepared a testcase with more sophisticated usage example

--- changelogd/templates/entry.md
+++ changelogd/templates/entry.md
@@ -1,1 +1,2 @@
 * {% if issue_id is defined and issue_id %}[#{{ issue_id }}]({{ issues_url }}/{{ issue_id }}): {% endif %}{{ message }}  
+
--- tests/test_changelogd.py
+++ tests/test_changelogd.py
@@ -1,9 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Tests for `changelogd` package."""
+import datetime
+import glob
+import os
+from pathlib import Path
+
 from click.testing import CliRunner
 
 from changelogd import cli
+from changelogd import commands
+from changelogd import config
+
+DRAFT = """# Changelog  
+
+## beta-version (2020-02-02)  
+
+This is a test release.
+
+
+### Features  
+* [#101](http://repo/issues/101): Another test feature  
+* [#100](http://repo/issues/100): Test feature  
+
+
+### Bug fixes  
+* [#102](http://repo/issues/102): Bug fixes  
+
+
+### Documentation changes  
+* [#103](http://repo/issues/103): Documentation update  
+
+  
+"""
+
+
+class FakeDate(datetime.date):
+    _date = None
+
+    @classmethod
+    def today(cls) -> datetime.date:
+        if not cls._date:
+            return super().today()
+        return cls._date
+
+    @classmethod
+    def set_date(cls, date: datetime.date) -> None:
+        cls._date = date
 
 
 def test_command_line_interface():
@@ -15,3 +58,54 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.cli, ["--help"])
     assert help_result.exit_code == 0
     assert "Show this message and exit." in help_result.output
+
+
+def test_full_flow(tmpdir, monkeypatch):
+    monkeypatch.setattr(config, "DEFAULT_PATH", Path(tmpdir) / "changelog.d")
+    monkeypatch.chdir(tmpdir)
+    monkeypatch.setattr(datetime, "date", FakeDate)
+    FakeDate.set_date(datetime.date(2020, 2, 2))
+
+    runner = CliRunner()
+
+    # start with init
+    init = runner.invoke(commands.init)
+    assert init.exit_code == 0
+    assert _list_directory(tmpdir) == [
+        "changelog.d/config.yaml",
+        "changelog.d/releases/.gitkeep",
+        "changelog.d/templates/entry.md",
+        "changelog.d/templates/main.md",
+        "changelog.d/templates/release.md",
+    ]
+
+    # add some entries
+    _create_entry(runner, "1", "100", "Test feature")
+    _create_entry(runner, "1", "101", "Another test feature")
+    _create_entry(runner, "2", "102", "Bug fixes")
+    _create_entry(runner, "3", "103", "Documentation update")
+    assert (
+        len(glob.glob((Path(tmpdir) / "changelog.d" / "*entry.yaml").as_posix())) == 4
+    )
+
+    # try draft release
+    draft = runner.invoke(commands.draft, ["beta-version"], "This is a test release.")
+    assert draft.exit_code == 0
+    output = draft.stdout[len("Release description (hit ENTER to omit): ") :]
+    assert output == DRAFT
+
+
+def _list_directory(directory):
+    output = []
+    for root, dirs, files in os.walk(directory):
+        for file_ in files:
+            output.append((Path(root) / file_).relative_to(directory).as_posix())
+
+    return output
+
+
+def _create_entry(runner, type, issue_id, message):
+    entry = runner.invoke(
+        commands.entry, input=os.linesep.join([type, issue_id, message])
+    )
+    assert entry.exit_code == 0
