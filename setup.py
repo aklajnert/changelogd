f6345429ed461aa51698ff3d898ce97048b3b44a
Andrzej Klajnert <github@aklajnert.pl>
2020-01-16 18:40:56 +0100

    Update develompent status classifier

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     author_email="python@aklajnert.pl",
     python_requires=">=3.6",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
