27522c9caf72589f57fcabc83d5f431fe661099a
Andrzej Klajnert <github@aklajnert.pl>
2020-01-17 16:07:15 +0100

    Add `publish` and `create_dist` steps to noxfile, remove tox.ini.

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,7 @@
 import nox
 
+nox.options.sessions = ["tests", "flake8", "mypy", "docs"]
+
 
 @nox.session(python=["3.6", "3.7", "3.8"])
 def tests(session):
@@ -8,7 +10,7 @@ def tests(session):
 
 
 @nox.session
-def flak8e(session):
+def flake8(session):
     session.install("flake8")
     session.run("flake8", "changelogd")
 
@@ -23,3 +25,20 @@ def mypy(session):
 def docs(session):
     session.install(".[docs]")
     session.run("sphinx-build", "-b", "html", "docs", "docs/_build", "-v", "-W")
+
+
+@nox.session
+def create_dist(session):
+    session.install("twine")
+    session.run("python", "setup.py", "sdist", "bdist_wheel")
+    session.run("twine", "check", "dist/*")
+
+
+@nox.session
+def publish(session):
+    """Publish to pypi. Run `nox publish -- prod` to publish to the official repo."""
+    create_dist(session)
+    twine_command = ["twine", "upload", "dist/*"]
+    if "prod" not in session.posargs:
+        twine_command.extend(["--repository-url", "https://test.pypi.org/legacy/"])
+    session.run(*twine_command)
