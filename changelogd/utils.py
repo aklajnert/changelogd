32f7bd2e797019bb2249680a830a9327e2e6ff7e
Andrzej Klajnert <github@aklajnert.pl>
2020-01-18 15:08:20 +0100

    Create function to add files to git

--- changelogd/utils.py
+++ changelogd/utils.py
@@ -1,6 +1,7 @@
 import logging
 import subprocess
 import typing
+from pathlib import Path
 
 
 def get_git_data() -> typing.Optional[typing.Tuple[str, str]]:
@@ -19,3 +20,11 @@ def get_git_data() -> typing.Optional[typing.Tuple[str, str]]:
         )
     }
     return data.get("user.name", ""), data.get("user.email", "")
+
+
+def add_to_git(path: typing.Union[Path, str]) -> None:
+    try:
+        subprocess.check_call(["git", "add", str(path)])
+        logging.info(f"Added to git: {path}")
+    except subprocess.CalledProcessError:
+        logging.error(f"Failed to add to git: {path}")
