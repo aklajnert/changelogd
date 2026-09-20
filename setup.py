7a54c90c7b729e75e98038fadb9b34d5bc6e4585
Andrzej Klajnert <github@aklajnert.pl>
2026-09-20 16:07:46 +0000

    Drop support for Python 3.8

--- setup.py
+++ setup.py
@@ -39,16 +39,13 @@ docs_requirements = [
 setup(
     author="Andrzej Klajnert",
     author_email="python@aklajnert.pl",
-    python_requires=">=3.6",
+    python_requires=">=3.9",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
